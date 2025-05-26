53d65b20c03d0e394192e308bd8b9b67845a1a28
CompanyUser <1101642762@qq.com>
2025-05-26 18:04:50 +0800

    svg矢量图展示配置

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,15 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import path from 'path'
+//矢量图 SVG
+import {createSvgIconsPlugin} from "vite-plugin-svg-icons";
+
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [
+    vue(),
+    createSvgIconsPlugin({
+    iconDirs :[path.resolve(__dirname,'src/assets/icons')],
+  })],
 })
