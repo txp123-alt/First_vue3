413af495de4ded299c5e6a8978dc42c51070b6d7
CompanyUser <1101642762@qq.com>
2025-05-26 18:10:57 +0800

    引入 elementPlus

--- src/App.vue
+++ src/App.vue
@@ -2,6 +2,9 @@
   <div>
     <h1>app</h1>
     <timeIcon name="time" width="100px" height="100px"></timeIcon>
+    <h1>测试elementUI</h1>
+    <el-button type="primary">按钮</el-button>
+    <el-button type="success">按钮</el-button>
   </div>
 </template>
 
--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,19 @@
 import { createApp } from 'vue'
-import './style.css'
+// import './style.css'
 import App from './App.vue'
 
+const app = createApp(App)
+
+//svg矢量图配置， 报错暂时不用管
 import 'virtual:svg-icons-register'
 
-createApp(App).mount('#app')
+//1. ++++++++++++++++++ 引入 Element Plus ++++++++++++++++++
+import ElementPlus from 'element-plus'
+import 'element-plus/dist/index.css'
+import zhCn from 'element-plus/es/locale/lang/zh-cn' //国际化的配置
+
+app.use(ElementPlus, {
+  locale: zhCn,
+})
+
+app.mount('#app')
