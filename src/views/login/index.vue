57baf8174768e2618c55b90a9bd3a4cea9868957
CompanyUser <1101642762@qq.com>
2025-05-27 17:02:04 +0800

    登录页静态搭建

--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -1,11 +1,60 @@
 <template>
-    <div>
-        <h1>我是一级路由，登录页面</h1>
+    <div class="login_container">
+        <el-row>
+            <el-col :span="12" :xs="0"></el-col>
+            <el-col :span="12" :xs="24">
+                <el-form class="login_form">
+                    <div></div>
+                    <h1>Hello</h1>
+                    <h2>欢迎来到测试环境</h2>
+                    <el-form-item>
+                        <el-input :prefix-icon="User" v-model="loginForm.username"></el-input>
+                    </el-form-item>
+                    <el-form-item>
+                        <el-input type="password" :prefix-icon="Lock" v-model="loginForm.password" show-password></el-input>
+                    </el-form-item>
+                    <el-form-item>
+                        <el-button class="login_btn" type="success" size="default">登录</el-button>
+                    </el-form-item>
+                </el-form>
+            </el-col>
+        </el-row>
     </div>
 </template>
 <script setup lang="ts">
+import {User,Lock} from '@element-plus/icons-vue'
+import { reactive } from 'vue';
 
+//收集登录信息
+let loginForm = reactive({
+    username: '',
+    password: ''
+})
 </script>
 <style lang="scss" scoped>
-
+.login_container{
+    width: 100%;
+    height: 100vh;
+    background: url('@/assets/images/background.jpg') no-repeat;
+    .login_form{
+        position: relative;
+        width: 80%;
+        top: 30vh;
+        background: url('@/assets/images/formBackGround.jpg');
+        background-size: cover;
+        padding: 40px;
+        h1{
+          color: white;
+          font-size: 40px;    
+        }
+        h2{
+          color: white;
+          font-size: 20px;    
+          margin: 20px 0px;
+        }
+        .login_btn{
+            width: 100%;
+        }
+    }
+}
 </style>
