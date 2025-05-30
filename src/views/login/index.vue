81cccecef8337637ac5eb2acd54ab71fa6cdf88d
CompanyUser <1101642762@qq.com>
2025-05-30 17:32:57 +0800

    表单验证不通过时，通过await来拦截请求

--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -3,14 +3,14 @@
         <el-row>
             <el-col :span="12" :xs="0"></el-col>
             <el-col :span="12" :xs="24">
-                <el-form class="login_form">
+                <el-form class="login_form" :model="loginForm" :rules="rules" ref="loginFormRef">
                     <div></div>
                     <h1>Hello</h1>
                     <h2>欢迎来到测试环境</h2>
-                    <el-form-item>
+                    <el-form-item prop="username">
                         <el-input :prefix-icon="User" v-model="loginForm.username"></el-input>
                     </el-form-item>
-                    <el-form-item>
+                    <el-form-item prop="password">
                         <el-input type="password" :prefix-icon="Lock" v-model="loginForm.password" show-password></el-input>
                     </el-form-item>
                     <el-form-item>
@@ -40,10 +40,26 @@ let loginForm = reactive({
     password: '123456'
 })
 
+//表单校验的配置对象
+const rules = reactive({
+    username:[
+        {required:true,min:3,max:10,message:'用户名长度在3-10个字符之间',trigger:'change'}
+    ],
+    password:[
+        {required:true,min:3,max:10,message:'密码长度在3-10个字符之间',trigger:'change'}
+    ]
+})
+
 //定义登录按钮状态
 let loginButLoading = ref(false)
 
+//获取表单的实例
+let loginFormRef = ref()
+
 const login = async() =>{
+    //保证全部表单校验通过
+    await loginFormRef.value.validate()
+    
     loginButLoading.value = true
     try {
         //可以写.then()的语法
